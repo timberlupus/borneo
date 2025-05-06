e9a44e14cfc3f0e07fab62870f5b8a5ac25d805d
Li Wei <oldrev@gmail.com>
2025-05-06 21:07:31 +0800

    change default pwm freq to 19k

--- fw/lyfi/main/src/rmtpwm.c
+++ fw/lyfi/main/src/rmtpwm.c
@@ -13,7 +13,7 @@
 
 static const char* TAG = "rmtpwm";
 
-#define RMTPWM_FREQ_HZ 24000 // 24 kHz PWM
+#define RMTPWM_FREQ_HZ 19000 // 19 kHz PWM
 #define RMT_PWM_RESOLUTION_HZ 10000000 // 10MHz resolution
 
 typedef struct {
