27dc0475daf6f1c21638bab82a41dd6668111cef
Li Wei <oldrev@gmail.com>
2025-07-26 18:43:45 +0800

    refactor: Add missing import for WidgetTester in test_helpers.dart

--- client/test/helpers/test_helpers.dart
+++ client/test/helpers/test_helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 Widget createTestWidget(Widget child) {
   return MaterialApp(home: Scaffold(body: child));
