876bafee0a2faa4d18b8d171afaa9742c50d2357
Wei Li <oldrev@gmail.com>
2025-11-09 01:19:41 +0800

    fix: Simplify device name checks in k_device_get_binding function

--- fw/components/drvfx/kernel/device.c
+++ fw/components/drvfx/kernel/device.c
@@ -30,28 +30,18 @@ bool k_device_is_ready(const struct drvfx_device* dev)
 
 const struct drvfx_device* k_device_get_binding(const char* name)
 {
-	const struct drvfx_device *dev;
-
-	/* A null string identifies no device.  So does an empty
-	 * string.
-	 */
-	if ((name == NULL) || (name[0] == '\0')) {
+    if ((name == NULL) || (name[0] == '\0')) {
         ESP_LOGE(TAG, "k_device_get_binding called with NULL or empty name");
         return NULL;
     }
 
-    /* Split the search into two loops: in the common scenario, where
-	 * device names are stored in ROM (and are referenced by the user
-	 * with CONFIG_* macros), only cheap pointer comparisons will be
-	 * performed. Reserve string comparisons for a fallback.
-	 */
-	for (dev = _drvfx_device_start; dev != _drvfx_device_end; dev++) {
+    for (const struct drvfx_device* dev = _drvfx_device_start; dev < _drvfx_device_end; dev++) {
         if (k_device_is_ready(dev) && (dev->name == name)) {
             return dev;
         }
     }
 
-    for (dev = _drvfx_device_start; dev != _drvfx_device_end; dev++) {
+    for (const struct drvfx_device* dev = _drvfx_device_start; dev < _drvfx_device_end; dev++) {
         if (k_device_is_ready(dev) && (strcmp(name, dev->name) == 0)) {
             return dev;
         }
