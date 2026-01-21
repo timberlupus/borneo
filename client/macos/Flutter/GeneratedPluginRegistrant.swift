1295c53a3522d3e718272d9b5aac7507247e6813
Wei Li <oldrev@gmail.com>
2026-01-21 18:15:22 +0800

    feat: Update plugin registrations and bump package versions in pubspec.lock

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flutter_timezone
 import geolocator_apple
 import nsd_macos
 import package_info_plus
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 import window_size
@@ -21,7 +20,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   NsdMacosPlugin.register(with: registry.registrar(forPlugin: "NsdMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
